2016ec0f32c848be144bed54297025f53cd2475d
Jean Leduc <jean.leduc80@gmail.com>
2023-03-08 17:43:38 +0100

    exo 10

--- index.php
+++ index.php
@@ -4,42 +4,38 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/exercice10.css">
     <title>Document</title>
 </head>
 <body>
-    
+    <h1 style="text-align: center;color:orange">Exercice 10 sur le JavaScript</h1>
+    <h3 style="text-align: center;color:orange">EventListener</h3>
+    <div class="divA">
+        <div id="MaDivNum1" class="divD">
+            coucou
+        </div>
+        <div id="Methode1" class="divD">
+            Clique moi dessus !
+        </div>
+        <div id="Methode2" class="divD">
+            Survole moi !
+        </div>
+        <div id="Methode3" class="divD" tabindex="0">
+            Appuie au clavier
+        </div>
+    </div>
+    <h3 style="text-align: center;color:orange">Première question</h3>
+    <div class="divA">
+        <div id="Methode4" class="class1">
+            Cette div changera de couleur si tu appuies dessus !
+        </div>
+    </div>
+    <h3 style="text-align: center;color:orange">Deuxième question</h3>
+    <div class="divA">
+        <div id="Methode5" class="class1">
+            Clique pour me faire disparaitre
+        </div>
+    </div>
+    <script type="text/javascript" src="js/exercice10.js"></script>
 </body>
-</html>
-
-
-
-<div class="MaDivNum1">Coucou</div>
-<div class="MaDivNum1">Coucou</div>
-<div class="MaDivNum1">Coucou</div>
-
-
-
-
-<script langage="javascript">
-    
-
-
-</script>
-
-
-<script src="main.js"></script>
-
-<script>
-    
-
-    myfunction();
-
-</script>
-
-
-<?php   
-    
-      echo "cc";
-      
-?>
-
+</html>  
