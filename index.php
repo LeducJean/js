1fb3c796ab2ab029bb5ef27b6a69d529d8c11b4b
Jean Leduc <jean.leduc80@gmail.com>
2023-03-08 16:17:53 +0100

    modif div

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="main.js"></script>
     <title>Document</title>
 </head>
 <body>
@@ -14,16 +13,32 @@
 
 
 
+<div class="MaDivNum1">Coucou</div>
+<div class="MaDivNum1">Coucou</div>
+<div class="MaDivNum1">Coucou</div>
+
+
+
+
 <script langage="javascript">
     
-    
- 
+
+
 </script>
 
 
+<script src="main.js"></script>
+
+<script>
+    
+
+    myfunction();
+
+</script>
+
 
 <?php   
-      
+    
       echo "cc";
       
 ?>
--- main.js
+++ main.js
@@ -1,28 +1,30 @@
-//var MavariableString = "toto";
-//var MavariableNumerique = 100;
-//var MavariableTableau = ['Apple', 'Banana'];
-//var MavariableObjet = window;
-//alert(toto + MavariableString + MavariableNumerique +
-//    MavariableTableau[0] + MavariableObjet.location.pathname);
+/*
+
+var MavariableString = "toto";
+var MavariableNumerique = 100;
+var MavariableTableau = ['Apple', 'Banana'];
+var MavariableObjet = window;
+alert(toto + MavariableString + MavariableNumerique +
+    MavariableTableau[0] + MavariableObjet.location.pathname);
 
 
 
-//var MavariableString = "toto" ;
-//var MavariableNumerique = prompt("entrez un nombre", 100);
-//
-//if (MavariableString == "toto" && MavariableNumerique !=200 || MavariableNumerique <= 100){
-//    alert("je suis le cas 1");
-//}else{
-//    alert("je suis le cas 2");
-//}
+var MavariableString = "toto" ;
+var MavariableNumerique = prompt("entrez un nombre", 100);
+
+if (MavariableString == "toto" && MavariableNumerique !=200 || MavariableNumerique <= 100){
+    alert("je suis le cas 1");
+}else{
+    alert("je suis le cas 2");
+}
 
 
 
-//if (window.outerWidth < 1250){
-//    alert("La fenetre est de largeur de moins de 1250px");
-//}else{
-//    alert("La fenetre est de largeur de plus de 1250px");
-//}
+if (window.outerWidth < 1250){
+    alert("La fenetre est de largeur de moins de 1250px");
+}else{
+   alert("La fenetre est de largeur de plus de 1250px");
+}
 
 
 
@@ -30,6 +32,41 @@ var k = prompt("ecrivez un nombre");
 var i = 1;
 for(var u=1; u<=k; u++){
     i = i*u;   
-   
+/  
 }
 alert("le résultat est = " +i);
+
+
+
+function myfunction()
+{
+    alert("Modification de la div");
+    var modification = document.getElementById ("MeDivNum1");
+    modification.innerHTML = "◘ ┤ î ╝ ▒ ┬ ‗ Í Ì ¨ £ ▼"
+}
+
+
+
+function myfunction()
+{
+
+    var modif = document.getElementByName("MaDivNum1");
+    modif.forEach(function(div)
+    {
+        duv.innerHTML = "◘ ┤ î ╝ ▒ ┬ ‗ Í Ì ¨ £ ▼";
+    });
+
+}
+
+*/
+
+function myfunction()
+{
+
+    var modif = document.getElementByName("MaDivNum1");
+    modif.forEach(function(div)
+    {
+        duv.innerHTML = "◘ ┤ î ╝ ▒ ┬ ‗ Í Ì ¨ £ ▼";
+    });
+
+}
