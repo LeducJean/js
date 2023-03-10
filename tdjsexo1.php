f75ccd8a1c6de1c2345ec18e18a8eabaeafc1cb1
Jean Leduc <jean.leduc80@gmail.com>
2023-03-10 14:56:19 +0100

    newfiles

---
+++ tdjsexo1.php
@@ -0,0 +1,27 @@
+<style>
+
+</style>
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Exo10</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+</head>
+
+
+
+<body>
+    <div id="Methode2" class="divD">Clic moi dessus</div>
+</body>
+
+
+
+<script src='main.js'></script>
+
+</html>
