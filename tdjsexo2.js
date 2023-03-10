1aa28a4346e2b692f26c01ddc85224ff2307c7c3
Jean Leduc <jean.leduc80@gmail.com>
2023-03-10 15:27:37 +0100

    exo2pi

--- tdjsexo2.js
+++ tdjsexo2.js
@@ -4,8 +4,8 @@ MaDiv.addEventListener("click", monclick);
 
 function monclick(evenement) {
     var dia = window.prompt("Saisir un diametre : ");
-    var perimetre = dia * 3.14;
+    var perimetre = dia * Math. PI;
     var air = dia / 2;
-    var air2 = 3.14 * air * air;
+    var air2 = Math. PI * air * air;
     alert("perimètre : " + perimetre + " air : " + air2);
 }
