7b27acd0471925cc76ec0116281e3cb93dff40a4
Jean Leduc <jean.leduc80@gmail.com>
2023-03-15 15:13:35 +0100

    javascript

---
+++ javascript.js
@@ -0,0 +1,11 @@
+var toto = "titi";
+
+function mafunction() {
+    setTimeout(fctfutur, 5000);
+
+    console.log("Ou etes vous ?");
+}
+
+function fctfutur() {
+    console.log("Je suis ici !");
+}
